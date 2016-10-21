67bca9669788998013eb6dc6c141edff05b996c5
Antonio Garinga <antonio.garinga@gmail.com>
2016-10-21 13:18:20 +0800

    made modifications on mypage.php

--- exercise4/mypage.php
+++ exercise4/mypage.php
@@ -97,7 +97,7 @@ INFORMATION
 <h id="Q5">BEST TRAIT</h>
 <button type="button" onclick="document.getElementById('Q5').innerHTML = 'SARCASM'">JUST CLICK THIS</button>
 
-<audio controls autoplay>
+<audio >
   
   <source src="Go Cozy - Glimmer - 02 Fruit Flies.mp3" type="audio/mpeg">
 </audio>
@@ -109,7 +109,7 @@ $name = $email = $gender = $comment = $website = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   if (empty($_POST["name"])) {
-    $nameErr = "Name is required";
+    $nameErr = "NAME IS REQUIRED ";
   } else {
     $name = test_input($_POST["name"]);
     // check if name only contains letters and whitespace
@@ -159,39 +159,28 @@ function test_input($data) {
 }
 ?>
 
-<h2>PHP Form Validation Example</h2>
-<p><span class="error">* required field.</span></p>
+<h3>WANT TO SEND A MESSAGE? SCROLL DOWN</h3>
+
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
   Name: <input type="text" name="name" value="<?php echo $name;?>">
   <span class="error">* <?php echo $nameErr;?></span>
-  <br><br>
+  
   E-mail: <input type="text" name="email" value="<?php echo $email;?>">
   <span class="error">* <?php echo $emailErr;?></span>
-  <br><br>
+  
   Website: <input type="text" name="website" value="<?php echo $website;?>">
   <span class="error"><?php echo $websiteErr;?></span>
-  <br><br>
+  
   Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
-  <br><br>
+  
   Gender:
   <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
   <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
   <span class="error">* <?php echo $genderErr;?></span>
-  <br><br>
+  
   <input type="submit" name="submit" value="Submit">  
 </form>
 
-<?php
-echo "<h2>Your Input:</h2>";
-echo $name;
-echo "<br>";
-echo $email;
-echo "<br>";
-echo $website;
-echo "<br>";
-echo $comment;
-echo "<br>";
-echo $gender;
-?>
+
 </body>
 </html>
