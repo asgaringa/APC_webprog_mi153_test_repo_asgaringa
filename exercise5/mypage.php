d048584ddf2fe08c870ad366444887216cfb2f19
Antonio Salvador Sebastian Garinga <antonio.garinga@gmail.com>
2016-11-05 13:16:37 +0800

    modified exer 5 final

--- exercise5/mypage.php
+++ exercise5/mypage.php
@@ -1,25 +1,22 @@
 <?php
-include_once 'dbconfig.php';
-if(isset($_POST['submit']))
-{
- // variables for input data
- $name = $_POST['name'];
- $nName = $_POST['nName'];
- $email = $_POST['email'];
- $hAd = $_POST['hAd'];
- $gender = $_POST['gender'];
- $cNum = $_POST['cNum'];
- $comment = $_POST['comment'];
- // variables for input data
- 
- // sql query for inserting data into database
- 
-        $sql_query = "INSERT INTO users(name,nName,email,hAd,gender,cNum,message) VALUES ('$name','$nName','$email','$hAd','$gender','$cNum','$comment')";
- mysqli_query($con,$sql_query);
-        
-        // sql query for inserting data into database
- 
-}
+	include_once 'dbconfig.php';
+	if(isset($_POST['submit']))
+		{
+			// variables for input data
+			$name = $_POST['name'];
+			$nName = $_POST['nName'];
+		    $email = $_POST['email'];
+			$hAd = $_POST['hAd'];
+			$gender = $_POST['gender'];
+			$cNum = $_POST['cNum'];
+			$comment = $_POST['comment'];
+			// variables for input data
+			
+			// sql query for inserting data into database			 
+			$sql_query = "INSERT INTO users(name,nName,email,hAd,gender,cNum,message) VALUES ('$name','$nName','$email','$hAd','$gender','$cNum','$comment')";
+			mysqli_query($con,$sql_query);
+			// sql query for inserting data into database
+		}
 ?>
 
 <html>
@@ -75,11 +72,7 @@ if(isset($_POST['submit']))
 				background-color: white;
 				color: red;
 			}
-			
-			audio{
-				display:none;
-			}
-			
+						
 			.input{
 				column-count: 2;
 				column-gap: 30px;
@@ -110,7 +103,7 @@ if(isset($_POST['submit']))
 	<body>
 		ANTONIO SALVADOR SEBASTIAN GARINGA </br>
 		<img src = "pic1.jpg" style = "width: 512px; height: 383px"></br>
-
+		
 		INFORMATION 
 
 			<table align ="center">
@@ -153,77 +146,72 @@ if(isset($_POST['submit']))
 		<h id="Q5">BEST TRAIT</h>
 		<button type="button" onclick="document.getElementById('Q5').innerHTML = 'SARCASM'">JUST CLICK THIS</button>
 
-
-		<audio > 
-		  <source src="Go Cozy - Glimmer - 02 Fruit Flies.mp3" type="audio/mpeg">
-		</audio>
-
 		<?php
-		$nameErr = $nNameErr = $emailErr = $genderErr = $cNumErr = $commentErr = "";
-		$name = $nName = $email = $hAd = $gender = $cNum = $comment = "";
+			$nameErr = $nNameErr = $emailErr = $genderErr = $cNumErr = $commentErr = "";
+			$name = $nName = $email = $hAd = $gender = $cNum = $comment = "";
 
-		if ($_SERVER["REQUEST_METHOD"] == "POST") {
-		  if (empty($_POST["name"])) {
-			$nameErr = "NAME IS REQUIRED ";
-		  } else {
-			$name = test_input($_POST["name"]);
-			if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
-			  $nameErr = "ONLY LETTERS ARE ALLOWED DUDE"; 
+			if ($_SERVER["REQUEST_METHOD"] == "POST") {
+				if (empty($_POST["name"])) {
+				$nameErr = "NAME IS REQUIRED ";
+			} else {
+				$name = test_input($_POST["name"]);
+				if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+				$nameErr = "ONLY LETTERS ARE ALLOWED DUDE"; 
+				}
 			}
-		  }
 		  
-		  if (empty($_POST["nName"])) {
-			$nNameErr = "NICKNAME IS REQUIRED ";
-		  } else {
-			$nName = test_input($_POST["nName"]);
-			if (!preg_match("/^[a-zA-Z ]*$/",$nName)) {
-			  $nNameErr = "ONLY LETTERS ARE ALLOWED DUDE"; 
+			if (empty($_POST["nName"])) {
+				$nNameErr = "NICKNAME IS REQUIRED ";
+			} else {
+				$nName = test_input($_POST["nName"]);
+				if (!preg_match("/^[a-zA-Z ]*$/",$nName)) {
+				$nNameErr = "ONLY LETTERS ARE ALLOWED DUDE"; 
+				}
 			}
-		  }
 		  
-		  if (empty($_POST["email"])) {
-			$emailErr = "E-MAIL IS REQUIRED MAYNE";
-		  } else {
-			$email = test_input($_POST["email"]);
-			if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-			  $emailErr = "INVALID EMAIL FORMAT MAYNE"; 
+			if (empty($_POST["email"])) {
+				$emailErr = "E-MAIL IS REQUIRED MAYNE";
+			} else {
+				$email = test_input($_POST["email"]);
+				if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+				$emailErr = "INVALID EMAIL FORMAT MAYNE"; 
+				}
 			}
-		  }
 		  
-		  if (empty($_POST["hAd"])) {
-			$hAd = "";
-		  } else {
-			$hAd = test_input($_POST["hAd"]);
-		  }
-		  
-		   if (empty($_POST["gender"])) {
-			$genderErr = "GENDER IS REQUIRED";
-		  } else {
-			$gender = test_input($_POST["gender"]);
-		  }
-		  
-		  if (empty($_POST["cNum"])) {
-			$cNumErr = "NUMBER IS REQUIRED";
-		  } else {
-			$cNum = test_input($_POST["cNum"]);
-			if (!filter_var($cNum, FILTER_VALIDATE_INT) === FALSE) {
-			  $cNumErr = "NUMBERS ONLY MAYNE"; 
+			if (empty($_POST["hAd"])) {
+				$hAd = "";
+			} else {
+				$hAd = test_input($_POST["hAd"]);
 			}
-		  }
 		  
-		  if (empty($_POST["comment"])) {
-			$comment = "";
-		  } else {
-			$comment = test_input($_POST["comment"]);
-		  }
-		}
+			if (empty($_POST["gender"])) {
+				$genderErr = "GENDER IS REQUIRED";
+			} else {
+				$gender = test_input($_POST["gender"]);
+			}
+		  
+			if (empty($_POST["cNum"])) {
+				$cNumErr = "NUMBER IS REQUIRED";
+			} else {
+				$cNum = test_input($_POST["cNum"]);
+				if (!filter_var($cNum, FILTER_VALIDATE_INT) === FALSE) {
+				$cNumErr = "NUMBERS ONLY MAYNE"; 
+				}
+			}
+		  
+			if (empty($_POST["comment"])) {
+				$comment = "";
+			} else {
+				$comment = test_input($_POST["comment"]);
+				}
+			}
 
-		function test_input($data) {
-		  $data = trim($data);
-		  $data = stripslashes($data);
-		  $data = htmlspecialchars($data);
-		  return $data;
-		}
+			function test_input($data) {
+			$data = trim($data);
+			$data = stripslashes($data);
+			$data = htmlspecialchars($data);
+			return $data;
+			}
 		?>
 
 		<h3>WANT TO SEND A MESSAGE? SCROLL DOWN</h3>
@@ -231,29 +219,34 @@ if(isset($_POST['submit']))
 		<form method="post" >
 			<div class="input">
 				<div class="col1">
-					NAME:<input type="text"  name="name" value="<?php echo $name;?>">
-					<span class="error"> <?php echo $nameErr;?></span>
-					<br><br>
-					NICKNAME:<input type="text" name="nName" value="<?php echo $nName;?>">
-					<span class="error"> <?php echo $nNameErr;?></span>
-					<br><br>
-					E-MAIL: <input type="text" name="email" value="<?php echo $email;?>">
-					<span class="error"> <?php echo $emailErr;?></span>
-					<br><br>
-					ADDRESS: <textarea name="hAd" rows="3" cols="35"><?php echo $hAd;?></textarea>
+					NAME:
+						<input type="text"  name="name" value="<?php echo $name;?>">
+						<span class="error"> <?php echo $nameErr;?></span>
+						<br><br>
+					NICKNAME:
+						<input type="text" name="nName" value="<?php echo $nName;?>">
+						<span class="error"> <?php echo $nNameErr;?></span>
+						<br><br>
+					E-MAIL: 
+						<input type="text" name="email" value="<?php echo $email;?>">
+						<span class="error"> <?php echo $emailErr;?></span>
+						<br><br>
+					ADDRESS: 
+					<textarea name="hAd" rows="3" cols="35"><?php echo $hAd;?></textarea>
 				</div>
 				<div class="col2">
 					GENDER:
-					<input type="radio" name="gender" value="Female"/>FEMALE
-					<input type="radio" name="gender" value="Male"/>MALE
-					<span class="error"> <?php echo $genderErr;?></span>
-					
-					<br><br>
-					CELLPHONE NUMBER:<input type="cNum" name="cNum" value="<?php echo $cNum;?>">
-					<span class="error"> <?php echo $cNumErr;?></span>
-					<br><br>
-					COMMENT: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
-					<br><br>
+						<input type="radio" name="gender" value="FEMALE"/>FEMALE
+						<input type="radio" name="gender" value="MALE"/>MALE
+						<span class="error"> <?php echo $genderErr;?></span>					
+						<br><br>
+					CELLPHONE NUMBER:
+						<input type="cNum" name="cNum" value="<?php echo $cNum;?>">
+						<span class="error"> <?php echo $cNumErr;?></span>
+						<br><br>
+					COMMENT:
+						<textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
+						<br><br>
 				</div>
 			</div>
 			
