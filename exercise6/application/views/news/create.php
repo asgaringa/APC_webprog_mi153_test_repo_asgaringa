858aed74b1c1ca862b0da2370e61508490d32c51
Antonio Salvador Sebastian Garinga <antonio.garinga@gmail.com>
2016-12-18 16:18:43 +0800

    modified something in create.php

--- exercise6/application/views/news/create.php
+++ exercise6/application/views/news/create.php
@@ -3,40 +3,46 @@
 		font-family: simplifica;
 		text-align: center;
 	}
+	
 	table{
 		font-size: 38px;
 	}
+	
 	input{
 		font-size: 38px;
 	}
+	
 	.button{
 		background-color: white;
-					color: black;
-					border: white;
-					font-family: simplifica;
-					font-size: 32px;
-					text-align: center;
+		color: black;
+		border: white;
+		font-family: simplifica;
+		font-size: 32px;
+		text-align: center;
 	}
-	.button:hover{
-					background-color: white;
-					color: red;
-				}
 	
+	.button:hover{
+		background-color: white;
+		color: red;
+	}	
 </style>
+
 <h1>SEND A MESSAGE HERE</h1>
- 
-<?php echo validation_errors(); ?>
- 
-<?php echo form_open('news/create'); ?>    
+
+	<?php echo validation_errors(); ?>
+	<?php echo form_open('news/create'); ?>
+	
     <table align = center>
         <tr>
             <td><label for="Name">Name</label></td>
             <td><input type="text" name="name" rows="10" cols="40"></input></td>
         </tr>
+		
         <tr>
             <td><label for="Nickname">Nickname</label></td>
             <td><input type="text" name="nName" rows="10" cols="40"></input></td>
         </tr>
+		
 		<tr>
             <td><label for="Email">Email</label></td>
             <td><input type="text" name="email" rows="10" cols="40"></input></td>
