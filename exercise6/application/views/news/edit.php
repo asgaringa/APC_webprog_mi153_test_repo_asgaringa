809c1dd3651f1a7cc2a0f3912506bfecb34d3c62
Antonio Salvador Sebastian Garinga <antonio.garinga@gmail.com>
2016-12-18 15:55:24 +0800

    modified edit.php

--- exercise6/application/views/news/edit.php
+++ exercise6/application/views/news/edit.php
@@ -6,31 +6,31 @@
 	<table>
         <tr>
             <td><label for="Name">Name</label></td>
-            <td><textarea name="Name" rows="10" cols="40"><?php echo $news_item['Name'] ?></textarea></td>
+            <td><input type="text" name="name" rows="10" cols="40"><?php echo $news_item['name'] ?></input></td>
         </tr>
         <tr>
             <td><label for="Nickname">Nickname</label></td>
-            <td><textarea name="Nickname" rows="10" cols="40"><?php echo $news_item['Nickname'] ?></textarea></td>
+            <td><input type="text" name="nName" rows="10" cols="40"><?php echo $news_item['nName'] ?></input></td>
         </tr>
 		<tr>
             <td><label for="Email">Email</label></td>
-            <td><textarea name="Email" rows="10" cols="40"><?php echo $news_item['Email'] ?></textarea></td>
+            <td><input type="text" name="email" rows="10" cols="40"><?php echo $news_item['email'] ?></input></td>
         </tr>
 		<tr>
             <td><label for="Home_Address">Home Address</label></td>
-            <td><textarea name="Home_Address" rows="10" cols="40"><?php echo $news_item['Home_Address'] ?></textarea></td>
+            <td><input type="text" name="hAd" rows="10" cols="40"><?php echo $news_item['hAd'] ?></input></td>
         </tr>
 		<tr>
             <td><label for="Gender">Gender</label></td>
-            <td><textarea name="Gender" rows="10" cols="40"><?php echo $news_item['Gender'] ?></textarea></td>
+            <td><input type="text" name="gender" rows="10" cols="40"><?php echo $news_item['gender'] ?></input></td>
         </tr>
 		<tr>
             <td><label for="Cp_Num">Cellphone number</label></td>
-            <td><textarea name="Cp_Num" rows="10" cols="40"><?php echo $news_item['Cp_Num'] ?></textarea></td>
+            <td><input type="text" name="cNum" rows="10" cols="40"><?php echo $news_item['cNum'] ?></input></td>
         </tr>
 		<tr>
             <td><label for="Comment">Comment</label></td>
-            <td><textarea name="Comment" rows="10" cols="40"><?php echo $news_item['Comment'] ?></textarea></td>
+            <td><input type="text" name="message" rows="10" cols="40"><?php echo $news_item['message'] ?></input></td>
         </tr>
         <tr>
             <td></td>
