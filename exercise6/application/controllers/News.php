2d4310fbd2bca0bc3e73737470961890a56848ad
Antonio Salvador Sebastian Garinga <antonio.garinga@gmail.com>
2016-12-18 15:53:46 +0800

    modified news.php

--- exercise6/application/controllers/News.php
+++ exercise6/application/controllers/News.php
@@ -39,13 +39,13 @@ class News extends CI_Controller {
  
         $data['title'] = 'Create a news item';
  
-        $this->form_validation->set_rules('Name', 'Name', 'required');
-        $this->form_validation->set_rules('Nickname', 'Nickname', 'required');
-		$this->form_validation->set_rules('Email', 'Email', 'required');
-        $this->form_validation->set_rules('Home_Address', 'Home address', 'required');
-		$this->form_validation->set_rules('Gender', 'Gender', 'required');
-        $this->form_validation->set_rules('CP_num', 'Cellphone number');
-		$this->form_validation->set_rules('Comment', 'Comment', 'required');
+        $this->form_validation->set_rules('name', 'name', 'required');
+        $this->form_validation->set_rules('nName', 'nName', 'required');
+		$this->form_validation->set_rules('email', 'email', 'required');
+        $this->form_validation->set_rules('hAd', 'hAd', 'required');
+		$this->form_validation->set_rules('gender', 'gender', 'required');
+        $this->form_validation->set_rules('cNum', 'cNum');
+		$this->form_validation->set_rules('message', 'comment', 'required');
  
         if ($this->form_validation->run() === FALSE)
         {
@@ -54,12 +54,11 @@ class News extends CI_Controller {
             $this->load->view('templates/footer');
  
         }
-        else
+       else
         {
-            $this->news_model->set_news();
-            $this->load->view('templates/header', $data);
-            $this->load->view('news/success');
-            $this->load->view('templates/footer');
+            $this->news_model->set_news($User_ID);
+            //$this->load->view('news/success');
+            redirect( base_url() . 'index.php/news');
         }
     }
     
@@ -78,13 +77,13 @@ class News extends CI_Controller {
         $data['title'] = 'Edit a news item';        
         $data['news_item'] = $this->news_model->get_news_by_id($User_ID);
         
-        $this->form_validation->set_rules('Name', 'Name', 'required');
-        $this->form_validation->set_rules('Nickname', 'Nickname', 'required');
-		$this->form_validation->set_rules('Email', 'Email', 'required');
-        $this->form_validation->set_rules('Home_Address', 'Home address', 'required');
-		$this->form_validation->set_rules('Gender', 'Gender', 'required');
-        $this->form_validation->set_rules('CP_num', 'Cellphone number');
-		$this->form_validation->set_rules('Comment', 'Comment', 'required');
+        $this->form_validation->set_rules('name', 'name', 'required');
+        $this->form_validation->set_rules('nName', 'nName', 'required');
+		$this->form_validation->set_rules('email', 'email', 'required');
+        $this->form_validation->set_rules('hAd', 'hAd', 'required');
+		$this->form_validation->set_rules('gender', 'gender', 'required');
+        $this->form_validation->set_rules('cNum', 'cNum');
+		$this->form_validation->set_rules('message', 'message', 'required');
  
         if ($this->form_validation->run() === FALSE)
         {
