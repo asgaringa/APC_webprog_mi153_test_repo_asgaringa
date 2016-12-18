277788334c1fc7808fce85cbcd099db5ff6b79d2
Antonio Salvador Sebastian Garinga <antonio.garinga@gmail.com>
2016-12-18 15:54:55 +0800

    modified create.php

--- exercise6/application/views/news/create.php
+++ exercise6/application/views/news/create.php
@@ -1,40 +1,65 @@
-<h2><?php echo $title; ?></h2>
+<style>
+	table,h1,input{
+		font-family: simplifica;
+		text-align: center;
+	}
+	table{
+		font-size: 38px;
+	}
+	input{
+		font-size: 38px;
+	}
+	.button{
+		background-color: white;
+					color: black;
+					border: white;
+					font-family: simplifica;
+					font-size: 32px;
+					text-align: center;
+	}
+	.button:hover{
+					background-color: white;
+					color: red;
+				}
+	
+</style>
+<h1>SEND A MESSAGE HERE</h1>
  
 <?php echo validation_errors(); ?>
  
 <?php echo form_open('news/create'); ?>    
-    <table>
+    <table align = center>
         <tr>
             <td><label for="Name">Name</label></td>
-            <td><textarea name="Name" rows="10" cols="40"></textarea></td>
+            <td><input type="text" name="name" rows="10" cols="40"></input></td>
         </tr>
         <tr>
             <td><label for="Nickname">Nickname</label></td>
-            <td><textarea name="Nickname" rows="10" cols="40"></textarea></td>
+            <td><input type="text" name="nName" rows="10" cols="40"></input></td>
         </tr>
 		<tr>
             <td><label for="Email">Email</label></td>
-            <td><textarea name="Email" rows="10" cols="40"></textarea></td>
+            <td><input type="text" name="email" rows="10" cols="40"></input></td>
         </tr>
 		<tr>
             <td><label for="Home_Address">Home Address</label></td>
-            <td><textarea name="Home_Address" rows="10" cols="40"></textarea></td>
+            <td><input type="text" name="hAd" rows="10" cols="40"></input></td>
         </tr>
 		<tr>
             <td><label for="Gender">Gender</label></td>
-            <td><textarea name="Gender" rows="10" cols="40"></textarea></td>
+            <td><input type="text" name="gender" rows="10" cols="40"></input></td>
         </tr>
 		<tr>
             <td><label for="Cp_Num">Cellphone number</label></td>
-            <td><textarea name="Cp_Num" rows="10" cols="40"></textarea></td>
+            <td><input type="text" name="cNum" rows="10" cols="40"></input></td>
         </tr>
 		<tr>
             <td><label for="Comment">Comment</label></td>
-            <td><textarea name="Comment" rows="10" cols="40"></textarea></td>
+            <td><input type="text" name="message" rows="10" cols="40"></input></td>
         </tr>
         <tr>
             <td></td>
-            <td><input type="submit" name="submit" value="Create news item" /></td>
+            <td><input type="submit" class="button" name="submit" value="SUBMIT" /></td>
         </tr>
     </table>    
 </form>
