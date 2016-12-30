4836cb20cf62bb7265afbed92d744ed3c08006c4
Antonio Salvador Sebastian Garinga <antonio.garinga@gmail.com>
2016-12-30 20:28:45 +0800

    Made the image show up

--- exercise7/basic/views/site/index.php
+++ exercise7/basic/views/site/index.php
@@ -7,9 +7,10 @@ $this->title = 'DARTH VADER';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>HI</h1>
+        <h2>ANTONIO SALVADOR SEBASTIAN GARINGA</h2>
 
-        <img src = "pic1.jpg" style = "width: 512px; height: 383px">
+        
+		<img src=<?php echo Yii::$app->request->baseUrl . "/Images/pic1.jpg";?> width=512px height=383px></a>
 
         <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">YII WEBSITE</a></p>
     </div>
